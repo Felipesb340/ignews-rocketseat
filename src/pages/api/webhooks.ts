83088e05f02e7c3e1578888984d6726482f992b6
Felipe Soares <felipesb340@hotmail.com>
2022-09-12 20:10:47 -0300

    Alteração para deploy

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -67,7 +67,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             await saveSubscription(
               checkoutSession.subscription.toString(),
               checkoutSession.customer.toString(),
-              true
+              false
             )
 
             break;
